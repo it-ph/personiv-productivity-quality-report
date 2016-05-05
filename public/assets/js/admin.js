64484b863643fd1c2ce84a858e0284325ab7c4c1
Marco Paco <marcopaco1230@gmail.com>
2016-05-06 00:50:29 +0800

    Fixed the scroll issue

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1815,6 +1815,14 @@ adminModule
 		}
 	}]);
 adminModule
+	.controller('notificationToastController', ['$scope', '$state', 'Preloader', function($scope, $state, Preloader){
+		$scope.notification = Preloader.getNotification();
+
+		$scope.viewNotification = function(){
+			$state.go($scope.notification.state, {'departmentID': $scope.notification.department_id});
+		};
+	}]);
+adminModule
 	.controller('addDepartmentDialogController', ['$scope', '$mdDialog', 'Preloader', 'Department', function($scope, $mdDialog, Preloader, Department){
 		$scope.department = {};
 		var busy = false;
@@ -2595,13 +2603,5 @@ adminModule
 			.success(function(data){
 				$scope.targets = data;
 			});
-	}]);
-adminModule
-	.controller('notificationToastController', ['$scope', '$state', 'Preloader', function($scope, $state, Preloader){
-		$scope.notification = Preloader.getNotification();
-
-		$scope.viewNotification = function(){
-			$state.go($scope.notification.state, {'departmentID': $scope.notification.department_id});
-		};
 	}]);
 //# sourceMappingURL=admin.js.map
